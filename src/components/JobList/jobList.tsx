a304c3bc722808b57711c379e994103489b7f13d
k-cicek <kevserrcicek@gmail.com>
2024-05-01 14:31:27 +0300

    Listedeki işleri düzenleme özelliği eklendi.

--- src/components/JobList/jobList.tsx
+++ src/components/JobList/jobList.tsx
@@ -1,24 +1,46 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '@/contexts/AppContext';
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { GrEdit } from "react-icons/gr";
+import { Job } from "@/lib/types";
+import ModalComponent from '../Modal/modal';
 
-const priorityColors = {
-    "Urgent": "#c82333",
-    "Regular": "#dfa800",
-    "Trivial": "#0168d9"
-};
 
 function JobList() {
     const ctx = useContext(AppContext);
+    const [selectedJob, setSelectedJob] = useState<Job | null>(null);
+    const [selectedPriority, setSelectedPriority] = useState<string | null>(null);
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
-    const sortedJobs = ctx.jobs.sort((a, b) =>
-        b.priority.localeCompare(a.priority)
+
+
+    const sortedJobs = ctx.jobs.sort((a: Job, b: Job) =>
+        ctx.priorities.indexOf(a.priority) - ctx.priorities.indexOf(b.priority)
     );
 
     const handleDelete = (name: string) => {
         if (window.confirm("Are you sure you want to delete this job?")) {
             ctx.deleteJob(name);
+        }
+    };
+
+    const openModal = (job: Job) => {
+        setSelectedJob(job);
+        setSelectedPriority(job.priority);
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const onSave = () => {
+        if (selectedJob && selectedPriority) {
+            const updatedJob = { ...selectedJob, priority: selectedPriority };
+            ctx.updateJob(updatedJob);
+            setIsModalOpen(false);
+        } else {
+            console.error('selectedJob or selectedPriority is null');
         }
     };
 
@@ -40,13 +62,22 @@ function JobList() {
                                 <button className={job.priority.toLowerCase()}>{job.priority}</button>
                             </td>
                             <td>
-                                <button><GrEdit /></button>
+                                <button onClick={() => openModal(job)}><GrEdit /></button>
                                 <button onClick={() => handleDelete(job.name)}><RiDeleteBin6Line /></button>
                             </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
+            <ModalComponent
+                isOpen={isModalOpen}
+                closeModal={closeModal}
+                job={selectedJob}
+                priority={selectedPriority}
+                setPriority={setSelectedPriority}
+                onSave={onSave}
+                priorities={ctx.priorities}
+            />
         </div>
     );
 }
--- src/components/Modal/modal.tsx
+++ src/components/Modal/modal.tsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import { Job } from '@/lib/types';
+import React from 'react';
+import Modal from 'react-modal';
+import Select from "react-select";
+import Input from "../Input";
 
-const modal = () => {
+interface ModalProps {
+    isOpen: boolean;
+    closeModal: () => void;
+    job: Job | null;
+    priority: string | null;
+    setPriority: (priority: string | null) => void;
+    onSave: () => void;
+    priorities: string[];
+}
+
+const ModalComponent: React.FC<ModalProps> = ({
+    isOpen,
+    closeModal,
+    job,
+    priority,
+    setPriority,
+    onSave,
+    priorities
+}) => {
     return (
-        <div>modal</div>
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={closeModal}
+            contentLabel="Job Modal"
+            className="Modal"
+            overlayClassName="Overlay"
+        >
+            <h2 className='modal-title'>New Job</h2>
+            <div className='modal-container'>
+                <div className='modal-input'>
+                    <Input
+                        label="Job Name"
+                        value={job?.name || ''}
+                        disabled={true}
+                    />
+                </div>
+                <div className='modal-select'>
+                    <span>Job Priority</span>
+                    <Select
+                        options={priorities.map((p) => ({ value: p, label: p }))}
+                        className="job-priority-select"
+                        placeholder="Choose"
+                        value={{ value: priority, label: priority }}
+                        onChange={(newValue) => {
+                            setPriority(newValue?.value ?? null);
+                        }}
+                        styles={{
+                            control: (baseStyles, state) => ({ ...baseStyles, height: 46 }),
+                        }}
+                    />
+                </div>
+                <div className='modal-buttons'>
+                    <button onClick={onSave}>Save</button>
+                    <button onClick={closeModal}>Cancel</button>
+                </div>
+            </div>
+        </Modal>
     )
 }
 
-export default modal
+export default ModalComponent;
